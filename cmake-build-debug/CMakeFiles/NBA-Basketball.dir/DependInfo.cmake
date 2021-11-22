c21acb7cc25cd880d662db49bcb979a43ce7111d
hiepton1234 <hiepton1234@gmail.com>
2021-11-22 03:43:02 -0800

    merged main to dev

---
+++ cmake-build-debug/CMakeFiles/NBA-Basketball.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Hiep/NBA-Basketball/DB/Database/DbManager.cpp" "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/CMakeFiles/NBA-Basketball.dir/DB/Database/DbManager.cpp.obj"
+  "C:/Users/Hiep/NBA-Basketball/ManageTeam/ManageTeams.cpp" "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/CMakeFiles/NBA-Basketball.dir/ManageTeam/ManageTeams.cpp.obj"
+  "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/NBA-Basketball_autogen/mocs_compilation.cpp" "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/CMakeFiles/NBA-Basketball.dir/NBA-Basketball_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/Hiep/NBA-Basketball/UI/ModdedLabel/ModdedLabel.cpp" "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/CMakeFiles/NBA-Basketball.dir/UI/ModdedLabel/ModdedLabel.cpp.obj"
+  "C:/Users/Hiep/NBA-Basketball/UI/login/login.cpp" "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/CMakeFiles/NBA-Basketball.dir/UI/login/login.cpp.obj"
+  "C:/Users/Hiep/NBA-Basketball/main.cpp" "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/CMakeFiles/NBA-Basketball.dir/main.cpp.obj"
+  "C:/Users/Hiep/NBA-Basketball/mainwindow.cpp" "C:/Users/Hiep/NBA-Basketball/cmake-build-debug/CMakeFiles/NBA-Basketball.dir/mainwindow.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MINGW_HAS_SECURE_API=1"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_SQL_LIB"
+  "QT_WIDGETS_LIB"
+  "UNICODE"
+  "WIN32"
+  "WIN64"
+  "_ENABLE_EXTENDED_ALIGNED_STORAGE"
+  "_UNICODE"
+  "_WIN32_WINNT=0x0601"
+  "_WIN64"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "NBA-Basketball_autogen/include"
+  "C:/Qt/6.2.1/mingw81_64/include/QtCore"
+  "C:/Qt/6.2.1/mingw81_64/include"
+  "C:/Qt/6.2.1/mingw81_64/mkspecs/win32-g++"
+  "C:/Qt/6.2.1/mingw81_64/include/QtWidgets"
+  "C:/Qt/6.2.1/mingw81_64/include/QtGui"
+  "C:/Qt/6.2.1/mingw81_64/include/QtSql"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
